01bb69db555268c2f9dae468bab2f4cbcd512930
Pablo Antonio <pabloa@gmail.com>
2013-09-22 20:14:41 -0300

    Update drawer so it draws a graph too

--- tp1a/drawer.py
+++ tp1a/drawer.py
@@ -1,26 +1,64 @@
 import sys
 import fileinput
+from sets import Set
 
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
+import pydot
 
 requesters = {}
+requesters_links = {}
 requested = {}
 repliers = {}
 replied = {}
 
+#
 # File processing
+#
 
 for line in fileinput.input():
     hwsrc, psrc, pdst, op = line.split()
     if op == 'who-has':
         requesters[psrc] = requesters.get(psrc, 0.0) + 1.0
+
+        if psrc not in requesters_links:
+            requesters_links[psrc] = Set([pdst])
+        else:
+            requesters_links[psrc].add(pdst)
+            
         requested[pdst] = requested.get(pdst, 0.0) + 1.0
     elif op == 'is-at':
         repliers[psrc] = repliers.get(psrc, 0.0) + 1.0
         replied[pdst] = replied.get(pdst, 0.0) + 1.0
-            
+#
 # Drawings
+#
+
+# Graph
+
+graph = pydot.Dot(graph_type='digraph')
+nodes = {}
+
+for psrc, pdsts in requesters_links.iteritems():
+    if psrc not in nodes:
+        nodes[psrc] = pydot.Node(psrc)
+        graph.add_node(nodes[psrc])
+
+    for pdst in pdsts:
+        if pdst not in nodes:
+            nodes[pdst] = pydot.Node(pdst)
+            graph.add_node(nodes[pdst])
+
+        edge = pydot.Edge(nodes[psrc], nodes[pdst])
+        graph.add_edge(edge)
+
+for psrc, n in requesters.iteritems():
+    nodes[psrc].set_width(str(n/9.0))
+    nodes[psrc].set_height(str(n/9.0))
+
+graph.write_png('test.png')
+
+# Bars
 
 # Necessary in order to make labels fit
 rcParams.update({'figure.autolayout': True})
@@ -33,5 +71,18 @@ def draw_IPs_barh(ips_dict, title, label):
     plt.title(title)
 
 draw_IPs_barh(requesters, 'IPs que hicieron request', 'pedidos')
+plt.savefig('requesters.png')
+plt.clf()
 
-plt.show()
+draw_IPs_barh(requested, 'IPs por las que se hizo request', 'pedidos')
+plt.savefig('requested.png')
+plt.clf()
+
+draw_IPs_barh(repliers, 'IPs que respondieron a un request', 'respuestas')
+plt.savefig('repliers.png')
+plt.clf()
+
+draw_IPs_barh(replied, 'IPs a las que se les respondio un request',
+    'respuestas')
+plt.savefig('replied.png')
+plt.clf()
